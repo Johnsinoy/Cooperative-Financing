a485bb8e55a7a4c990fbdfdb8236f982bf36b51f
John Carlo Sinoy <jcsinoy91@gmail.com>
2025-03-02 18:29:47 -0700

    Create the Area folder for the admin and user section including the files needed

---
+++ Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿ausing Microsoft.AspNetCore.Mvc;
+
+namespace CooperativeFinancing.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
