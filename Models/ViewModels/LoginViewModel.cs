ac518480ce2676458e9c0c96ade219e2d16b3475
John Carlo Sinoy <jcsinoy91@gmail.com>
2025-03-02 18:47:35 -0700

    small adjustment

---
+++ Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CooperativeFinancing.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string errorMessage { get; set; }
+
+    }
+}
